ff08cd7073b0aae9b6af6a58fa79b9da6d8cb4ed
Trygve Vang <trygve.n.vang@gmail.com>
2017-11-09 12:41:17 +0100

    Web-app can now get questions from Web-API

--- Flybilletter-oblig3-webapps/app/Content.ts
+++ Flybilletter-oblig3-webapps/app/Content.ts
@@ -14,5 +14,41 @@ export class Content {
     showFAQ: boolean;
     submitQ: boolean;
     allQuestions: Array<Question>;
+    form: FormGroup;
+    loading: boolean;
 
+    constructor(private _http: Http, private fb: FormBuilder) {
+        this.form = fb.group({
+            /*ID: [""],
+            Firstname: [null, Validators.compose([Validators.required, Validators.pattern("[a-zA-ZøæåØÆÅ\\-. ]{2,30}")])],
+            Lastname: [null, Validators.compose([Validators.required, Validators.pattern("[a-zA-ZøæåØÆÅ\\-. ]{2,30}")])],
+            Question: [null, Validators.compose([Validators.required, Validators.pattern("[a-zA-ZøæåØÆÅ]{2,}")])],
+            QuestionType: [] */
+        });
+    }
+
+    ngOnInit() {
+        this.loading = true;
+        this.showFAQ = true;
+        this.submitQ = false;
+        this.getAll();
+    }
+
+    getAll() {
+        this._http.get("api/Question")
+            .map(data => {
+                let jsonData = data.json();
+                return jsonData;
+            })
+            .subscribe(jsonData => {
+                this.allQuestions = [];
+                if (jsonData) {
+                    for (let question of jsonData) {
+                        this.allQuestions.push(question);
+                    }
+                    this.loading = false;
+                }
+            },
+            error => alert(error), () => console.log("All questions loaded (get-api/Question)."));
+    }
 }
